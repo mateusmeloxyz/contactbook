82dc0e6f822be4744d537d716be133f01341dadf
Mateus Melo <mateus@mateusmelo.com>
2026-01-16 15:19:44 -0300

    Add swagger, create generic repository

--- BackEnd/ContactBook/ContactBook.API/Program.cs
+++ BackEnd/ContactBook/ContactBook.API/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
